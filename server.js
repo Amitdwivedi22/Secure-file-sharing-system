23486f641c8daa1c4e44cfc1101ea9091dac6319
Amitdwivedi22 <adwivedi08340@gmail.com>
2025-04-02 23:11:46 +0530

    updated server.js

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const express =  require("express");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
+const upload = require("./upload.js"); 
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static("upload")); //Server upload the file if needed
 
 // middleware to verify
 
@@ -21,6 +23,19 @@ const verifyToken = (req, res, next) => {
         res.status(400).json({error: "Invalid Token"});
     }
 };
+
+//upload Route
+
+app.post("/upload", upload.single("file"),(req,res) => {
+    if(!req.file){
+        return res.status(400).json({message:"No file uploaded"})
+    }
+    res.json({
+        message:"File upload successfully",
+        filePath:`/upload/${req.file.filename}`
+    });
+
+});
 
 //This is Protected route it can only accessed if valid jwt token provided 
 
